8f06252dfe2e2a69b3f70fa30b6e7717faa31b59
wizardpasindu07 <67377569+wizardpasindu07@users.noreply.github.com>
2020-10-23 16:54:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -346,7 +346,6 @@ bot.on("message", async (message) => { // eslint-disable-line
             }
         });
         serverQueue.songs = [];
-        await.m.react("👋");
         serverQueue.connection.dispatcher.end("[runCmd] Stop command has been used");
         return message.channel.send({
             embed: {
